875d80e0a99d8ec6516cc9d96e3c4ad96f18fa00
eduardo-sl <eduardosl.dev@gmail.com>
2024-09-11 00:37:10 -0300

    feat: adds hello command and redis client for testing server integration

--- client/client_test.go
+++ client/client_test.go
@@ -4,30 +4,41 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"sync"
 	"testing"
 	"time"
+
+	"github.com/redis/go-redis/v9"
 )
 
+func TestNewClientRedisClient(t *testing.T) {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:5001",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
 
+	if err := rdb.Set(context.Background(), "key", "value", 0).Err(); err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println("WE ARE HERE")
+}
 
 func TestNewClient(t *testing.T) {
 	c, err := New("localhost:5001")
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer c.Close()
 	time.Sleep(time.Second)
-	for i := 0; i < 10; i++ {
-		fmt.Println("SET =>", fmt.Sprintf("bar_%d", i))
-		if err := c.Set(context.Background(), fmt.Sprintf("foo_%d", i), fmt.Sprintf("bar_%d", i)); err != nil {
-			log.Fatal(err)
-		}
-
-		val, err := c.Get(context.Background(), fmt.Sprintf("foo_%d", i))
-		if err != nil {
-			log.Fatal(err)
-
-		}
-		fmt.Println("GET =>", val)
+	if err := c.Set(context.Background(), "foo", "bar"); err != nil {
+		log.Fatal(err)
 	}
+
+	val, err := c.Get(context.Background(), "foo")
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	fmt.Println("GET =>", val)
 }
--- proto.go
+++ proto.go
@@ -3,15 +3,14 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"io"
-	"log"
 
 	resp "go-in-memory-store/resp"
 )
 
 const (
-	CommandSET = "SET"
-	CommandGET = "GET"
+	CommandSET   = "SET"
+	CommandGET   = "GET"
+	CommandHELLO = "hello"
 )
 
 type Command interface {
@@ -21,44 +20,21 @@ type SetCommand struct {
 	key, val []byte
 }
 
+type HelloCommand struct {
+	value string
+}
+
 type GetCommand struct {
 	key []byte
 }
 
-func parseCommand(raw string) (Command, error) {
-	rd := resp.NewReader(bytes.NewBufferString(raw))
-	for {
-		v, _, err := rd.ReadValue()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if v.Type() == resp.Array {
-			for _, value := range v.Array() {
-				switch value.String() {
-				case CommandGET:
-					if len(v.Array()) != 2 {
-						return nil, fmt.Errorf("invalid number of variables for GET command")
-					}
-					cmd := GetCommand{
-						key: v.Array()[1].Bytes(),
-					}
-					return cmd, nil
-				case CommandSET:
-					if len(v.Array()) != 3 {
-						return nil, fmt.Errorf("invalid number of variables for SET command")
-					}
-					cmd := SetCommand{
-						key: v.Array()[1].Bytes(),
-						val: v.Array()[2].Bytes(),
-					}
-					return cmd, nil
-				}
-			}
-		}
+func respWriteMap(m map[string]string) []byte {
+	buf := &bytes.Buffer{}
+	buf.WriteString("%" + fmt.Sprintf("%d\r\n", len(m)))
+	rw := resp.NewWriter(buf)
+	for k, v := range m {
+		rw.WriteString(k)
+		rw.WriteString(":" + v)
 	}
-	return nil, fmt.Errorf("invalid or unknown command received: %s", raw)
+	return buf.Bytes()
 }
